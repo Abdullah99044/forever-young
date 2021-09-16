19442b727e002aee5af5c90dc105b94cb92c0e9f
Abdullah <ab-benkathir@outlook.com>
2021-09-16 12:01:23 +0200

    tafelmanieren

---
+++ tafelmanieren/PrintDeTafel.py
@@ -0,0 +1,5 @@
+while True:
+    tafel = int(input("welke getal tafel wil je?:"))
+    for x in range(1,13):
+        print(tafel, "x" , x , "=", tafel * x)
+
---
+++ tafelmanieren/uren.py
@@ -0,0 +1,5 @@
+for x in range (-1, 13):
+        print(x , str("am"))
+for y in range(11,24):
+    print(y , str("pm"))
+        
